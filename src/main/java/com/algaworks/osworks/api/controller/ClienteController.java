9db7cf9543aeda68bc73711d238b8cf416aac518
felipe Januario <felipejanuario86@gmail.com>
2020-06-15 23:11:43 -0300

    Criando classe CLIENTEs com o mapeamento para gerar a lista na API

--- src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -1,15 +1,33 @@
 package com.algaworks.osworks.api.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.osworks.domain.model.Cliente;
 
 @RestController
 public class ClienteController {
 	
 	@GetMapping("/clientes")
-	public String lista() {
+	public List<Cliente> lista() {
 		
+		var cliente = new Cliente();
+		cliente.setId(1L);
+		cliente.setNome("João");
+		cliente.setTelefone("21 9999999");
+		cliente.setEmail("emailTeste@Gmail.com");
 		
-		return "Teste";
+		var cliente2 = new Cliente();
+		
+		cliente2.setId(2L);
+		cliente2.setNome("Maria");
+		cliente2.setTelefone("21 98888888");
+		cliente2.setEmail("mariadaSilva@Gmail.com");
+		
+		return Arrays.asList(cliente,cliente2);
 	}
 }
