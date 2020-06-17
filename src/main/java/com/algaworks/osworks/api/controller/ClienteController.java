b063558c487657cd041195b41cbaaa9645e5e3ac
felipe Januario <felipejanuario86@gmail.com>
2020-06-17 13:35:46 -0300

    @NotBlank e @Valid - Para validação da integridade do banco ao preenchimento dos campos

--- src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -57,12 +58,12 @@ public class ClienteController {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente adicionar(@RequestBody Cliente cliente){
+	public Cliente adicionar(@Valid @RequestBody Cliente cliente){
 		return clienteRepository.save(cliente);
 	}
 	
 	@PutMapping("/{clienteId}")
-	public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId,
+	public ResponseEntity<Cliente> atualizar(@Valid @PathVariable Long clienteId,
 		@RequestBody Cliente cliente){
 		
 		if(!clienteRepository.existsById(clienteId)) {
